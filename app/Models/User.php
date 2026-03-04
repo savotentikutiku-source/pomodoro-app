11cfed832ab9623779f437972b2260162ed19f55
savotentikutiku-source <savotentikutiku@gmail.com>
2026-03-04 08:46:51 +0100

    Add HasApiTokens to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    // ★2. ここに HasApiTokens を追加！（部品を実際に装備する）
+    use HasApiTokens, HasFactory, Notifiable;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
