b27fb68b0eef8375616a42a33955f852f932386e
savotentikutiku-source <savotentikutiku@gmail.com>
2026-03-05 06:18:13 +0100

    Fix Models:PomoRecord: add model use and user_id

--- app/Models/PomoRecord.php
+++ app/Models/PomoRecord.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PomoRecord extends Model
 {
-    //
+    use HasFactory;
+
+    // ★ここ！ここに 'user_id' や 'pomo_count' は入っていますか？
+    protected $fillable = [
+        'user_id',
+        'task_name',
+        'pomo_count',
+    ];
 }
